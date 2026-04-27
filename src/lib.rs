e0e386f99d92a759582be97acba0a1dfb6928d9a
Diankes <122510601+Diankes@users.noreply.github.com>
2026-04-27 11:42:12 -0600

    - Update IMUEntry struct with better stubs about the expected values - Add load_csv to take the csv path and load into a vector of structs - Add serde macros to properly adapt snake case keys

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use serde::Deserialize;
+use std::error::Error;
 
 /// Opting for Clap derive option since it is more idiomatic and allows to type the file path
 #[derive(Parser, Debug)]
@@ -22,8 +24,41 @@ struct Args {
     #[arg(short, long, verbatim_doc_comment)]
     file: std::path::PathBuf,
 }
+#[derive(Debug, Deserialize)]
+pub struct IMUEntry {
+    pub ts: f64,
+    pub id: String,
+    #[serde(rename = "Ax")]
+    pub ax: f64,
+    #[serde(rename = "Ay")]
+    pub ay: f64,
+    #[serde(rename = "Az")]
+    pub az: f64,
+    #[serde(rename = "Gx")]
+    pub gx: f64,
+    #[serde(rename = "Gy")]
+    pub gy: f64,
+    #[serde(rename = "Gz")]
+    pub gz: f64,
+}
 
-pub fn run() {
+impl IMUEntry {
+    fn load_csv(path: std::path::PathBuf) -> Result<Vec<IMUEntry>, Box<dyn Error>> {
+        let mut imu_reader = csv::Reader::from_path(path)?;
+
+        // collect requires explicit type hint because it is generic
+        let imu_records: Vec<IMUEntry> = imu_reader
+            .deserialize()
+            .collect::<Result<Vec<IMUEntry>, csv::Error>>()?;
+        Ok(imu_records)
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    println!("{}", args.file.to_string_lossy());
+    let imu_readings = IMUEntry::load_csv(args.file)?;
+    for imu_reading in imu_readings.iter().take(5) {
+        println!("{:#?}", imu_reading);
+    }
+    Ok(())
 }
