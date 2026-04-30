853d6b0e48fc825001049f647566a03d0e5ab1a8
Diankes <122510601+Diankes@users.noreply.github.com>
2026-04-29 20:12:47 -0600

    Move data on *how the domain is accessed* to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use clap::Parser;
+
+/// Opting for Clap derive option since it is more idiomatic and allows to type the file path
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about = "Flight controller log parser",
+    long_about = "Apogee: Flight Controller log parser"
+)]
+struct Args {
+    /// Path to the input CSV file
+    ///
+    /// The headers of the file must be in the following format:
+    ///
+    ///   ts      - timestamp (seconds)
+    ///   id      - sensor identifier (e.g. IMU0)
+    ///   Ax,Ay,Az - accelerometer X/Y/Z (m/s²)
+    ///   Gx,Gy,Gz - gyroscope X/Y/Z (rad/s)
+    ///
+    /// Example:
+    ///   -0.755, IMU0, -9.62, -1.59, -0.91, -0.42, -0.21, 1.12
+    #[arg(short, long, verbatim_doc_comment)]
+    file: std::path::PathBuf,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    apogee::run()?;
+    let args = Args::parse();
+    apogee::run(args.file)?;
     Ok(())
 }
